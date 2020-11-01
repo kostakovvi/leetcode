bdbddb73eba995e6eded4d1c894a8963528a0506
kostakovvi <kostakovvi@gmail.com>
2020-11-01 18:08:04 +0300

    Add reverseInteger task and tests

--- leetcode/easy/reverseInteger.cpp
+++ leetcode/easy/reverseInteger.cpp
@@ -1,12 +1,20 @@
 #include <easy/reverseInteger.h>
+#include <limits>
 
 int reverseInteger(int x)
 {
 	int result = 0;
 	while (x != 0)
 	{
+		const int currentSymbol = x % 10;
+		if ((result > INT_MAX / 10 || (result == INT_MAX / 10 && currentSymbol > 7)) ||
+			(result < INT_MIN / 10 || (result == INT_MIN / 10 && currentSymbol < -8)))
+		{
+			result = 0;
+			break;
+		}
 		result = result * 10 + x % 10;
-		x = x / 10;
+		x /= 10;
 	}
 
 	return result;
--- leetcodeTest/testReverseInteger.cpp
+++ leetcodeTest/testReverseInteger.cpp
@@ -5,6 +5,16 @@ TEST(TestReverseInteger, Test1)
 {
 	const int test123 = 123;
 	const int test321 = 321;
+	const int testM123 = -123;
+	const int testM321 = -321;
+	const int test0 = 0;
+	const int test1534236469 = 1534236469;
+
 	EXPECT_EQ(reverseInteger(test123), test321);
+	EXPECT_EQ(reverseInteger(testM123), testM321);
+	EXPECT_EQ(reverseInteger(INT_MAX), test0);
+	EXPECT_EQ(reverseInteger(INT_MIN), test0);
+	EXPECT_EQ(reverseInteger(test0), test0);
+	EXPECT_EQ(reverseInteger(test1534236469), test0);
 	EXPECT_TRUE(true);
 }
